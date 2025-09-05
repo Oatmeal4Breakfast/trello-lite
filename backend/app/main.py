fbc79008348eebe9d2f7ebbf54d83ad19c9cf2ae
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2025-09-04 23:12:04 -0400

    update the packages to include pydantic[email]

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
 # backend/app/main.py
 from fastapi import FastAPI
+from app.routers import boards, lists, cards, users
+
 
 app = FastAPI()
+app.include_router(boards.router)
+app.include_router(lists.router)
+app.include_router(cards.router)
+app.include_router(users.router)
 
 @app.get("/")
 def read_root():
